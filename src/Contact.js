f962e7d4509e108443bcd8c036d6f2d2603f12be
Ryan Sproule <rsproule23@gmail.com>
2017-06-23 16:45:39 -0400

    contact page

--- src/Contact.js
+++ src/Contact.js
@@ -21,7 +21,7 @@ class Contact extends Component{
 
 						<li>
 						 	<a href="https://www.linkedin.com/in/ryan-sproule-00990381/">
-						 	<img alt="linkedin" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinkedIn_Logo.svg/1000px-LinkedIn_Logo.svg.png"/>
+						 	<img className="github" alt="linkedin" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinkedIn_Logo.svg/1000px-LinkedIn_Logo.svg.png"/>
 							</a>
 						</li>
 
@@ -32,7 +32,22 @@ class Contact extends Component{
 							</a>
 						</li>
 
-						
+						<li>
+							<a href="mailto:rsproule23@gmail.com">
+							<img alt="email" src={require("./Social_Media_Icons/PNG/Circle_Black_White/gmail.png")}/>
+								<h2 className="email">rsproule23@gmail.com</h2>
+							</a>
+						</li>
+
+						<li>
+							<a href="mailto:rsproule@wustl.edu">
+								<img alt="email" src="http://upe.wustl.edu/images/wustl.png"/>
+								<h2 className="email">rsproule@wustl.edu</h2>
+							</a>
+						</li>
+
+
+
 					</ul>
 				</div>
 			</div>
